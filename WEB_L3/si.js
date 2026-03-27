70a38827b663f873487b9a1ea0ee5cf2a9efeea4
HeroicXoX <aadityatimilsina2024ind@gmail.com>
2026-03-27 22:38:20 +0530

    Added WEB_L3 JavaScript lab

---
+++ WEB_L3/si.js
@@ -0,0 +1,6 @@
+function calc(){
+var p=parseFloat(document.getElementById("p").value);
+var r=parseFloat(document.getElementById("r").value);
+var t=parseFloat(document.getElementById("t").value);
+o.innerHTML=(p*r*t)/100;
+}
