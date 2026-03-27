fd34cf493840000d1cb591fd81d1e24ec921b2cf
HeroicXoX <aadityatimilsina2024ind@gmail.com>
2026-03-27 23:09:52 +0530

    Fixed LA3_11 external JS issue

--- WEB_L3/si.js
+++ WEB_L3/si.js
@@ -1,6 +1,14 @@
-function calc(){
-var p=parseFloat(document.getElementById("p").value);
-var r=parseFloat(document.getElementById("r").value);
-var t=parseFloat(document.getElementById("t").value);
-o.innerHTML=(p*r*t)/100;
+function calc() {
+    var p = parseFloat(document.getElementById("p").value);
+    var r = parseFloat(document.getElementById("r").value);
+    var t = parseFloat(document.getElementById("t").value);
+
+    if (isNaN(p) || isNaN(r) || isNaN(t)) {
+        alert("Enter valid values");
+        return;
+    }
+
+    var si = (p * r * t) / 100;
+
+    document.getElementById("o").innerHTML = "Simple Interest = " + si;
 }
